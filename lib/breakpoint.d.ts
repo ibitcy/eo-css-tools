01db9206ca70524744b0b25339723b99b25b26ed
Ruslan Shashkov <rshashkov@icloud.com>
2020-01-29 02:34:24 +0200

    Upd libs

--- lib/breakpoint.d.ts
+++ lib/breakpoint.d.ts
@@ -1,10 +1,8 @@
 declare type IValue = number | string | undefined;
-declare type ICssString = (cssTemplateString: TemplateStringsArray, ...values: any) => string;
 interface IBreakpointParams {
     min?: IValue;
     max?: IValue;
 }
-export declare function breakpoint(params: IBreakpointParams | IValue[]): {
-    css: ICssString;
-};
+export declare function breakpoint(params: IBreakpointParams | IValue[]): string;
+export declare const brp: typeof breakpoint;
 export {};
